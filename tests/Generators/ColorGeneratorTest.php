211a071f6a3bb86974c483dcfce8cffcd9130113
MATHEUS SOARES <matheus_onlive@live.com>
2025-01-23 16:49:34 -0300

    feature: Gerador de cores hexadecimais implementado

---
+++ tests/Generators/ColorGeneratorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ForgeBits\FabricaDeFakes\Tests\Generators;
+
+use ForgeBits\FabricaDeFakes\Container\DefaultContainer;
+use ForgeBits\FabricaDeFakes\Generators\Colors\ColorGeneratorInterface;
+use ForgeBits\FabricaDeFakes\Generators\Email\EmailGeneratorInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class ColorGeneratorTest extends TestCase
+{
+    private ColorGeneratorInterface $generator;
+
+    /**
+     * @throws NotFoundExceptionInterface|ContainerExceptionInterface
+     */
+    public function __construct(string $name)
+    {
+        $this->generator = DefaultContainer::createContainer()->get('colorGenerator');
+
+        parent::__construct($name);
+    }
+
+    public function testGenerateColor()
+    {
+        $color = $this->generator->hexadecimalColor();
+
+        $this->assertIsString($color);
+        $this->assertMatchesRegularExpression('/^#[0-9A-F]{6}$/', $color);
+    }
+}
